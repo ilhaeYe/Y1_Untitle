af5760ba26b94c856316cc4e25dc4a2f80257c88
예일해 <romanceduck@yeilhaeui-MacBook-Pro.local>
2018-02-10 21:23:56 +0900

    Initial Commit

---
+++ Y1_Untitle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Y1_Untitle
+//
+//  Created by 예일해 on 2018. 2. 10..
+//  Copyright © 2018년 RomanceDuck. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
